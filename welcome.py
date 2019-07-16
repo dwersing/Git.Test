15ca06dc6744b83e6e45ee38b8fb762dae971444
dwersing <42800092+dwersing@users.noreply.github.com>
2019-07-16 14:08:22 -0400

    Added welcome.py

---
+++ welcome.py
@@ -0,0 +1,8 @@
+new_customer = input('Welcome!  Would you like a pizza?')
+if new_customer[0].lower() == 'y':
+    print('Great!')
+else:
+    print('What the heck are you doing in a pizza shop?!')
+
+
+
